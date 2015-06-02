a531d41660a17c780cb4b72100e07bdce8a36597
Simeon Manolov <simeon.manolov@sumup.com>
2015-06-02 14:27:05 +0300

    Redeem `include_dirs` configuration

--- linter.py
+++ linter.py
@@ -12,28 +12,21 @@ class Elixirc(Linter):
     syntax = ("elixir")
 
     executable = "elixirc"
-    tempfile_suffix = "ex"
+    tempfile_suffix = "-"
 
-    # Must skip lines in the stack trace such as
-    #
-    #     |    (stdlib) lists.erl:1336: :lists.foreach/2
-    #
-    # because the line number leads to array index out of bound exception.
-    #
-    # Since they all start with 4 spaces, just ignore all lines starting with a space.
     regex = (
-        r"^[^ ].+:(?P<line>\d+):"
+        r"(?:\*+\s\(.+\) )?(?P<filename>.+):(?P<line>\d+):"
         r"(?:(?P<warning>\swarning:\s)|(?P<error>\s))"
         r"(?P<message>.+)"
     )
 
     defaults = {
+        "include_dirs": [],
         "pa": []
     }
 
     def cmd(self):
-        """Override to accept option `pa`."""
-
+        """Override to accept options `include_dirs` and `pa`."""
         tmpdir = os.path.join(tempfile.gettempdir(), 'SublimeLinter3')
         command = [
             self.executable_path,
@@ -43,9 +36,31 @@ class Elixirc(Linter):
         ]
 
         settings = self.get_view_settings()
+        dirs = settings.get('include_dirs', [])
         paths = settings.get('pa', [])
 
         for p in paths:
             command.extend(["-pa", p])
 
+        for d in dirs:
+            command.extend(["-r", "%s/**/*.ex" % d])
+
         return command
+
+    def split_match(self, match):
+        """
+        Return the components of the match.
+
+        We override this because unrelated library files can throw errors,
+        and we only want errors from the linted file.
+
+        """
+        if match:
+            # The linter seems to always change its working
+            # dir to that of the linted given file, so the
+            # reported error will contain a basename only.
+            if match.group('filename') != os.path.basename(self.filename):
+                match = None
+
+        return super().split_match(match)
+
